2678b31e3b1608a46b48acfbd27b820878cb150d
Imran Khan <imrankhan772244@gmail.com>
2024-02-25 13:08:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Login from './components/Login'
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header />
+        <Routes>
+        
+          <Route  path='/' element={<Login />} />
+        
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
