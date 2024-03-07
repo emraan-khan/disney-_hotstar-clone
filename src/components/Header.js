62917cbe5955c7a78405ddfb05489e8ff07d7270
Imran Khan <imrankhan772244@gmail.com>
2024-03-07 22:24:17 +0530

    home update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 
 import { UseDispatch,useDispatch,useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { selectUserName,selectUserPhoto, setUserLoginDetails } from '../features/users/userSlice';
+import { selectUserName,selectUserPhoto, setUserLoginDetails,setSignOutState } from '../features/users/userSlice';
 
 const Header = (props) => {
 
@@ -13,7 +13,9 @@ const Header = (props) => {
   const navigate=useNavigate();
   const userName=useSelector(selectUserName)
   // const [userName,setUserName]=useState('Imran');
-  const userPhoto=useSelector(selectUserPhoto)
+  const userPhoto=useSelector(selectUserPhoto);
+  console.log(userPhoto);
+
   
   if(userName){
     console.log(userName);
@@ -24,13 +26,13 @@ const Header = (props) => {
       if(user){
         setUser(user);
         navigate("/home");
-        
       }
     })
   },[userName])
 
   const handleAuth=()=>{
-    signInWithPopup(auth,provider)
+    if(!userName){
+    auth.signInWithPopup(auth,provider)
       .then((result) => {
         console.log(result);
         setUser(result.user)
@@ -38,6 +40,12 @@ const Header = (props) => {
       .catch((error) => {
         alert(error.message);
       })
+    }else if(userName){
+      auth.signOut().then(()=>{
+        dispatch(setSignOutState());
+        navigate('/');
+      }).catch((err)=> alert(err.message))
+    }
   }
 
   const setUser=(user)=>{
@@ -94,7 +102,12 @@ const Header = (props) => {
       <span>SERIES</span>
       </a>
       </NavMenu>
+      <SignOut>
       <UserImg src={userPhoto} alt={userName}/>
+      <DropDown>
+        <span onClick={handleAuth}>Sign Out</span>
+      </DropDown>
+      </SignOut>
       </>
       }
       </Nav>
@@ -214,4 +227,41 @@ const UserImg = styled.img`
 height: 100%;
 `;
 
-export default Header
+const DropDown=styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  boder: 1px solid rgba(151, 151,151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+const SignOut=styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg}{
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover{
+    ${DropDown}{
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
+export default Header;
